c442a862ef6d9b9e17e98f4cea2dc98450ee1da6
David Ivy <davidivy003@gmail.com>
2016-09-21 09:55:12 -0400

    Rename lambda files to clarify their functionality

---
+++ app/lambda/transformer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Wraggler = require('./app/src/wraggler');
+
+exports.handler = (event, context, callback) => {
+  var body = event;
+  console.log(body);
+  var output = Wraggler.flatten(body.data);
+
+  callback(null, {
+    data: output
+  });
+};
